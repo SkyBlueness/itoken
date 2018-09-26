630b8e04ae0ae91d5f908bebc67c4d6fb2c46fdd
gejinwei <995127604@qq.com>
2018-09-27 03:43:07 +0800

    测试一下

--- itoken-config/src/main/java/com/gejinwei/itokenconfig/ItokenConfigApplication.java
+++ itoken-config/src/main/java/com/gejinwei/itokenconfig/ItokenConfigApplication.java
@@ -3,13 +3,14 @@ package com.gejinwei.itokenconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 分布式配置中心
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 @EnableConfigServer   //开启配置中心
 public class ItokenConfigApplication {
 
