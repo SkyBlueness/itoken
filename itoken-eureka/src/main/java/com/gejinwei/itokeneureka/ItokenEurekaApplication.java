871bdf19b1a43a3dec12dbe036f1b09c40f60da1
gejinwei <995127604@qq.com>
2018-09-27 21:39:55 +0800

    测试一下

--- itoken-eureka/src/main/java/com/gejinwei/itokeneureka/ItokenEurekaApplication.java
+++ itoken-eureka/src/main/java/com/gejinwei/itokeneureka/ItokenEurekaApplication.java
@@ -2,6 +2,7 @@ package com.gejinwei.itokeneureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-
+@EnableEurekaClient
 public class ItokenEurekaApplication {
 
     public static void main(String[] args) {
