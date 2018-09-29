156fe1fae470e3b94613b602b8b167ea03247a31
gejinwei <995127604@qq.com>
2018-09-29 23:52:39 +0800

    测试一下

--- itoken-service-admin/src/main/java/com/gejinwei/itoken/service/admin/ItokenServiceAdminApplication.java
+++ itoken-service-admin/src/main/java/com/gejinwei/itoken/service/admin/ItokenServiceAdminApplication.java
@@ -1,9 +1,10 @@
 package com.gejinwei.itoken.service.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient
--- itoken-service-admin/src/test/java/com/gejinwei/itoken/service/admin/ItokenServiceAdminApplicationTests.java
+++ itoken-service-admin/src/test/java/com/gejinwei/itoken/service/admin/ItokenServiceAdminApplicationTests.java
@@ -1,16 +1,41 @@
 package com.gejinwei.itoken.service.admin;
 
+import com.gejinwei.itoken.service.admin.pojo.TbSysUser;
+import com.gejinwei.itoken.service.admin.service.AdminService;
+import com.gejinwei.itoken.service.admin.service.impl.AdminServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.UUID;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ItokenServiceAdminApplication.class)
 public class ItokenServiceAdminApplicationTests {
 
+    @Autowired
+    private AdminService adminService;
+
     @Test
-    public void contextLoads() {
+    public void adminResgister() {
+        TbSysUser tbSysUser = new TbSysUser();
+        tbSysUser.setUserCode(UUID.randomUUID().toString());
+        tbSysUser.setLoginCode("995127604@qq.com");
+        tbSysUser.setPassword("123456");
+        tbSysUser.setCreateDate(new Date());
+        tbSysUser.setUserName("gejinwei");
+        tbSysUser.setUserType("0");
+        tbSysUser.setMgrType("1");
+        tbSysUser.setCreateBy(tbSysUser.getLoginCode());
+        tbSysUser.setUpdateBy(tbSysUser.getLoginCode());
+        tbSysUser.setUpdateDate(new Date());
+        tbSysUser.setCorpCode("0");
+        tbSysUser.setCorpName("123");
+        adminService.register(tbSysUser);
+
     }
 
 }
