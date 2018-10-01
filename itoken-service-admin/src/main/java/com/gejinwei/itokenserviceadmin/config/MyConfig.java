2dc788a576ed39dd31c5e127c3aff1f23bb38173
gejinwei <995127604@qq.com>
2018-10-01 09:10:46 +0800

    测试一下

--- itoken-service-admin/src/main/java/com/gejinwei/itokenserviceadmin/config/MyConfig.java
+++ itoken-service-admin/src/main/java/com/gejinwei/itokenserviceadmin/config/MyConfig.java
@@ -1,18 +1,18 @@
-package com.gejinwei.itokenserviceadmin.config;
-
-import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class MyConfig {
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource(){
-        return new DruidDataSource();
-    }
-}
+//package com.gejinwei.itokenserviceadmin.config;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class MyConfig {
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource(){
+//        return new DruidDataSource();
+//    }
+//}
