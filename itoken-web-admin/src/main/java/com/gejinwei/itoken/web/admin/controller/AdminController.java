5f3f878424af60347841ba919b2203d4a56fabfb
gejinwei <995127604@qq.com>
2018-10-01 09:50:06 +0800

    开启熔断服务

--- itoken-web-admin/src/main/java/com/gejinwei/itoken/web/admin/controller/AdminController.java
+++ itoken-web-admin/src/main/java/com/gejinwei/itoken/web/admin/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.gejinwei.itoken.web.admin.controller;
 
+import com.gejinwei.itoken.web.admin.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AdminController {
 
+    @Autowired
+    AdminService adminService;
+
     @RequestMapping(value = {"","login"},method = RequestMethod.GET)
     public String login(){
+        String json = adminService.login("", "");
+        System.out.println(json);
         return "index";
     }
 }
