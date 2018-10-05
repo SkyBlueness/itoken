bc1d52690e6a4819b4d21b87f3c350959a2d5779
gejinwei <995127604@qq.com>
2018-10-06 00:41:37 +0800

    完成单点登录

---
+++ itoken-service-ports/src/main/java/com/gejinwei/itoken/service/ports/ItokenServicePortsApplication.java
@@ -0,0 +1,12 @@
+package com.gejinwei.itoken.service.ports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenServicePortsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenServicePortsApplication.class, args);
+    }
+}
---
+++ itoken-service-ports/src/test/java/com/gejinwei/itoken/service/ports/ItokenServicePortsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gejinwei.itoken.service.ports;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItokenServicePortsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
